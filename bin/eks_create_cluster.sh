e1d2441d5d4c6339c0aaddcf8e9a4cf6c6fe2f71
Innovays <131834160+Innovays@users.noreply.github.com>
2023-05-30 19:16:46 +0200

    Update eks_create_cluster.sh

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -10,9 +10,9 @@ echo $CLUSTER_NAME
 echo
 kubectl config get-clusters | grep -q ${CLUSTER_NAME}
 echo
-echo eksctl get cluster --region=${REGION}
+eksctl get cluster --region=${REGION}
 echo 
-echo eksctl get cluster --region=${REGION} | grep -q ${CLUSTER_NAME}
+eksctl get cluster --region=${REGION} | grep -q ${CLUSTER_NAME}
 echo
 
 if kubectl config get-clusters | grep -q ${CLUSTER_NAME} || eksctl get cluster --region=${REGION} | grep -q ${CLUSTER_NAME}; then
