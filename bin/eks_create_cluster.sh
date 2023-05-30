326246a9503848c726658fcf2ec6ca80091aefa4
Innovays <131834160+Innovays@users.noreply.github.com>
2023-05-30 19:39:43 +0200

    Update eks_create_cluster.sh

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -4,18 +4,29 @@ CLUSTER="hello"
 REGION="us-west-2"
 CLUSTER_NAME="${CLUSTER}.${REGION}.eksctl.io"
 
-kubectl config get-clusters
-echo
-echo $CLUSTER_NAME
-echo
-kubectl config get-clusters | grep -q ${CLUSTER_NAME}
-echo
-eksctl get cluster --region=${REGION}
-echo 
-eksctl get cluster --region=${REGION} | grep -q ${CLUSTER_NAME}
-echo
+# kubectl config get-clusters
+# echo
+# echo $CLUSTER_NAME
+# echo
+# kubectl config get-clusters | grep -q ${CLUSTER_NAME}
+# echo
+# eksctl get cluster --region=${REGION}
+# echo 
+# eksctl get cluster --region=${REGION} | grep -q ${CLUSTER_NAME}
+# echo
 
-if kubectl config get-clusters | grep -q ${CLUSTER_NAME} || eksctl get cluster --region=${REGION} | grep -q ${CLUSTER_NAME}; then
+echo "Retrieving clusters..."
+CLUSTERS=$(kubectl config get-clusters)
+
+echo "Current clusters:"
+echo "$CLUSTERS"
+echo "Checking for $CLUSTER_NAME..."
+
+EKSCLT_CLUSTER=$(eksctl get cluster --region=${REGION} | grep ${CLUSTER_NAME})
+echo $?
+
+
+if kubectl config get-clusters | grep -q ${CLUSTER_NAME} || $EKSCLT_CLUSTER ; then
     echo
     echo "Cluster '${CLUSTER_NAME}' already exists!"
     echo
