3f7b384c7131a5da1e852a859211d447bf749f42
Innovays <131834160+Innovays@users.noreply.github.com>
2023-05-30 19:59:29 +0200

    Update eks_create_cluster.sh

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -30,8 +30,14 @@ if kubectl config get-clusters | grep -q ${CLUSTER_NAME} || $EKSCLT_CLUSTER ; th
     echo
     echo "Cluster '${CLUSTER_NAME}' already exists!"
     echo
+    if $EKSCLT_CLUSTER; then
+    echo
+    echo "Update kubeconfig..."
+    aws eks --region ${REGION} update-kubeconfig --name hello
 else
     echo
     echo "Creating cluster..."
     ./bin/eksctl create cluster --config-file=hello_cluster.yml
+    # Add the newly created EKS cluster to kubeconfig
+    aws eks --region ${REGION} update-kubeconfig --name ${CLUSTER}
 fi
