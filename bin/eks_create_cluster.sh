d12bf53c3f3adf0e78a90ff9929f2fc3f9a63109
Innovays <131834160+Innovays@users.noreply.github.com>
2023-05-31 15:37:45 +0200

    Update eks_create_cluster.sh

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -12,8 +12,9 @@ echo "$CLUSTERS"
 echo "Checking for $CLUSTER_NAME..."
 
 EKSCLT_CLUSTER=$(eksctl get cluster --region=${REGION} | grep ${CLUSTER_NAME})
+echo $EKSCLT_CLUSTER
 
-if kubectl config get-clusters | grep -q ${CLUSTER_NAME} || $EKSCLT_CLUSTER; then
+if kubectl config get-clusters | grep -q ${CLUSTER_NAME} || $EKSCLT_CLUSTER == "True" ; then
     echo
     echo "Cluster '${CLUSTER_NAME}' already exists!"
     echo
